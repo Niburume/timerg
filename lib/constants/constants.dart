b477a6e06bb7bcc509d5e0958120769dab4e13b7
NiburuMe <niburume@gmail.com>
2023-05-29 19:20:40 +0200

    start timer

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -6,6 +6,7 @@ const String tgreetingText = 'Welcome back!';
 const String tNickname = 'Nickname';
 const String tEmail = 'Email';
 const String tPassword = 'Password';
+const String someWord = 'aa';
 
 const Duration tRequestFrequency = Duration(seconds: 10);
 
